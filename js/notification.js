a370570b46641215744abe4727bb2e9c5ea710a4
Heidi Kussakawa <heidiks@gmail.com>
2015-10-29 18:48:14 -0200

    noti

--- js/notification.js
+++ js/notification.js
@@ -1,29 +1,33 @@
+        function notificacao() {
+            if (Notification.permission === "granted" && pontoHoje.horaSaida().subtract(10, 'minutes').isAfter(moment())) {
+                var notification = createNotification("minina", "18:13:05");
+                setTimeout(notification.close.bind(notification), 5000);
+            } else if (Notification.permission !== 'denied') {
+                Notification.requestPermission(function (permission) {
+                    if (permission === "granted" && pontoHoje.horaSaida().subtract(10, 'minutes').isAfter(moment()))  {
+                        var notification = createNotification("minina", "18:13:05");
+                        setTimeout(notification.close.bind(notification), 5000);
+                    }
+                });
+            } else 
+                setTimeout(notificacao, 60000);
+        }
 
-function notificacao() {
+        function createNotification(label, horario) {
+            var options = {
+                body: horario,
+                icon: 'http://www.db1.com.br/assets/images/logo.png'
+            }
 
-  if (!("Notification" in window)) 
-    alert("Oops! Este navegador não suporta notificações");
-  else if (Notification.permission === "granted") {
-    var notification = createNotification("minina", "18:13:05");
-    setTimeout(notification.close.bind(notification), 5000);
-  }
+            return new Notification("Jornada "+ label +" cumprida!", options);
+        }
 
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (permission === "granted")  {
-        var notification = createNotification("minina", "18:13:05");
-        setTimeout(notification.close.bind(notification), 5000);
-      }
-    });
-  }
-  setTimeout(notificacao, 60000);
-}
+        notificacao();
 
-function createNotification(label, horario) {
- var options = {
-      body: horario,
-      icon: 'http://www.db1.com.br/assets/images/logo.png'
-  }
-
-  return new Notification("Jornada "+ label +" cumprida!", options);
-}
+<div align="center">   
+  <h5>
+    <label>
+      <input type="checkbox" >Notificar quando jornada m&iacute;nima atingida
+    </label>
+  </h5>
+</div>
